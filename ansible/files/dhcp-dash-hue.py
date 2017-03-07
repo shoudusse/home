2fba15a8c79e6c228241c6b5080e88220f5fae7f
Sylvain Houdusse <sylvain.houdusse@orange.com>
2017-03-07 19:48:11 +0100

    Execute script for dash button

---
+++ ansible/files/dhcp-dash-hue.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import time
+import sys
+import syslog
+from qhue import Bridge
+
+# Variables for each Dash button MAC address
+kleenex_dash = 'ac:63:be:16:7b:89'
+
+bridge_addr = 'Philips-hue'
+bridge_addr = '10.27.0.176'
+
+
+def main(argv):
+
+  action = argv[1]
+  mac = argv[2]
+  ip = argv[3]
+  hostname = argv[4]
+
+  if action != 'add':
+    sys.exit(0)
+
+  if mac != kleenex_dash:
+    sys.exit(0)
+
+  syslog.syslog('Dash button action detected')
+
+  # Connect to Hue bridge
+  b = Bridge(bridge_addr, "NSJDk9QrxwB6JVhGPzMmbzj1myW77mvB1Xu66xEK")
+
+  # Variables for Hue lights
+  living_room = b.lights[1]
+  kitchen = b.lights[2]
+  bedroom = b.lights[3]
+
+
+  current_state = bedroom().get(u'state').get(u'on')
+  syslog.syslog('Bedroom light state was %s' % current_state)
+  new_state = not current_state
+  bedroom.state(on=new_state, bri=64)
+  syslog.syslog('Bedroom light state set to %s' % new_state)
+
+
+if __name__ == '__main__':
+  main(sys.argv)
