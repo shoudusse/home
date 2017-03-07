fd28a67949736e55576514b5bcd628afa5ae018c
Sylvain Houdusse <syh@slyweb.org>
2017-03-07 23:18:58 +0100

    Fix bug

--- ansible/files/dhcp-dash-hue.py
+++ ansible/files/dhcp-dash-hue.py
@@ -9,10 +9,11 @@ from qhue import Bridge
 kleenex_dash = 'ac:63:be:16:7b:89'
 
 bridge_addr = 'Philips-hue'
-bridge_addr = '10.27.0.176'
-
+last_action_file = '/tmp/dhcp-dash-hue'
 
 def main(argv):
+
+  syslog.syslog(str(argv))
 
   action = argv[1]
   mac = argv[2]
@@ -24,6 +25,31 @@ def main(argv):
     sys.exit(0)
 
   syslog.syslog('Dash button action detected')
+
+  try:
+    h = open(last_action_file, 'r')
+    lastseen = int(float(h.read().rstrip('\n')))
+    h.close()
+    if not lastseen:
+      lastseen = 0
+  except Exception:
+    syslog.syslog("Can't load state file, setting last seen at 0")
+    lastseen = 0
+
+  now = time.time()
+  syslog.syslog("Last seen: %i" % int(lastseen))
+  delta = int(now) - lastseen
+  syslog.syslog("Last seen: %i ago" % delta)
+
+  h = open(last_action_file, 'w')
+  h.write(str(now))
+  h.close()
+
+  if delta < 10:
+    syslog.syslog("No triggering action (double request?)")
+    sys.exit(0)
+
+  syslog.syslog("Triggering action")
 
   # Connect to Hue bridge
   b = Bridge(bridge_addr, "NSJDk9QrxwB6JVhGPzMmbzj1myW77mvB1Xu66xEK")
